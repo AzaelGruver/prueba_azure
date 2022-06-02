a741def1725f86da4e4128b71a8c4b47175f03db
Azael Garcia Jaimes <agarciaj@gruver.mx>
2022-06-02 11:33:02 -0500

    .

--- dist/routes/mecanicos.routes.js
+++ dist/routes/mecanicos.routes.js
@@ -11,6 +11,6 @@ var _mecanicosController = require("../controllers/mecanicosController");
 
 var router = (0, _express.Router)(); // obtener todos los mecanicos
 
-router.get('/mecanicos', _mecanicosController.obtenerMecanicos);
+router.get('/', _mecanicosController.obtenerMecanicos);
 var _default = router;
 exports["default"] = _default;
