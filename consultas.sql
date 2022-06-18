27419bf3bd74caa79a702bca3d78210aa71aa278
nayelyGruver <nperez@gruver.mx>
2022-06-18 10:28:56 -0500

    query del reporte sin consulta a el dato (horas trabajadas)

--- consultas.sql
+++ consultas.sql
@@ -5,7 +5,9 @@ NOTAS
 *No se encuentra el tiempo trabajado
 *No se encuentra la clasificacion del mecanico
 */
-SELECT DISTINCT mecanicos.no_empleado, mecanicos.nombre, mecanicos_suc.COSTO_HORA,
+SELECT DISTINCT 
+	mecanicos.nombre AS MECANICO, 
+	162.59 /*CONSULTA A HORAS*/ AS HORAS,
 	CASE 
 		WHEN mecanicos_suc.COSTO_HORA >= 110 then 'A'
 		WHEN  mecanicos_suc.COSTO_HORA >= 100 then 'B'
@@ -13,7 +15,11 @@ SELECT DISTINCT mecanicos.no_empleado, mecanicos.nombre, mecanicos_suc.COSTO_HOR
 		WHEN  mecanicos_suc.COSTO_HORA >= 70 then 'D'
 		WHEN  mecanicos_suc.COSTO_HORA >= 60 then 'E'
 		ELSE 'NO CLASIFICADO'
-	END AS Clasificacion
+	END AS CLASIFICACION,
+	mecanicos_suc.COSTO_HORA AS TARIFA, 
+	mecanicos_suc.COSTO_HORA * 162.59 /*CONSULTA A HORAS*/ AS SUELDO, 
+	190 AS OBJETIVO, 
+	ROUND(162.59  /*CONSULTA A HORAS*/ *100/190,0) AS ALCANCE
 	FROM mecanicos INNER JOIN mecanicos_suc 
 	ON mecanicos.no_empleado = mecanicos_suc.no_empleado 
 	WHERE mecanicos.no_empleado > 0
