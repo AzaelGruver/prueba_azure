7833873808d50e2af990f9ca25727c589dc7fb74
nayelyGruver <nperez@gruver.mx>
2022-06-20 12:49:48 -0500

    Consulta del reporte con las horas trabajadas en la quincena

--- consultas.sql
+++ consultas.sql
@@ -26,4 +26,23 @@ SELECT DISTINCT
 	WHERE mecanicos.no_empleado > 0
 
 
+/*Consulta de reporte con los campos; id_mec, nombre, costo_hora, horas quincenales*/
+SELECT mecanicos_suc.id_mec, mecanicos_suc.nombre,
+mecanicos_suc.COSTO_HORA,
+(select SUM(destajo_por_fecha_1.ttr)
+FROM destajo_por_fecha_1 
+WHERE destajo_por_fecha_1.fecha_fact BETWEEN '2022-05-01' AND '2022-05-05'
+AND destajo_por_fecha_1.id_mec = mecanicos_suc.id_mec) AS HORAS 
 
+FROM mecanicos_suc 
+JOIN destajo_por_fecha_1 on destajo_por_fecha_1.id_mec = mecanicos_suc.id_mec
+WHERE destajo_por_fecha_1.fecha_fact BETWEEN '2022-05-01' AND '2022-05-05'
+AND mecanicos_suc.no_empleado > 0
+GROUP BY mecanicos_suc.id_mec, mecanicos_suc.nombre, mecanicos_suc.COSTO_HORA
+ORDER BY mecanicos_suc.id_mec
+
+/*Suma las horas trabajadas por cada orden atendida por un mismo mecanico */
+(SELECT SUM(destajo_por_fecha_1.ttr) 
+FROM destajo_por_fecha_1 
+WHERE destajo_por_fecha_1.id_mec = 'OJV'
+GROUP BY destajo_por_fecha_1.no_orden, destajo_por_fecha_1.id_mec)
