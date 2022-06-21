fb6e080a568922db189415b9d2e3d028e3922f20
nayelyGruver <nperez@gruver.mx>
2022-06-21 16:19:44 -0500

    correcion query productivo

--- dist/controllers/mecanicosController.js
+++ dist/controllers/mecanicosController.js
@@ -5,7 +5,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.reporteMecanico = exports.obtenerMecanicos = exports.nominaMecanico = exports.nominaAllMecanicos = void 0;
+exports.reporteMecanico = exports.obtenerMecanicos = exports.nominaMecanico = exports.nominaAllMecanicos = exports.detallesNominaMecanico = void 0;
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
@@ -142,7 +142,7 @@ var nominaMecanico = /*#__PURE__*/function () {
 
 exports.nominaMecanico = nominaMecanico;
 
-var nominaAllMecanicos = /*#__PURE__*/function () {
+var detallesNominaMecanico = /*#__PURE__*/function () {
   var _ref4 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee4(req, res) {
     var pool, resultado;
     return _regenerator["default"].wrap(function _callee4$(_context4) {
@@ -156,7 +156,7 @@ var nominaAllMecanicos = /*#__PURE__*/function () {
           case 3:
             pool = _context4.sent;
             _context4.next = 6;
-            return pool.request().input("fechaInicio", req.params.fechaInicio).input("fechaFinal", req.params.fechaFinal).input("idPlaza", req.params.idPlaza).query(_database.querys.nominaAllMecanicos);
+            return pool.request().input("fechaInicio", req.params.fechaInicio).input("fechaFinal", req.params.fechaFinal).input("idMec", req.params.idMec).query(_database.querys.detallesNominaMecanico);
 
           case 6:
             resultado = _context4.sent;
@@ -176,8 +176,49 @@ var nominaAllMecanicos = /*#__PURE__*/function () {
     }, _callee4, null, [[0, 10]]);
   }));
 
-  return function nominaAllMecanicos(_x7, _x8) {
+  return function detallesNominaMecanico(_x7, _x8) {
     return _ref4.apply(this, arguments);
+  };
+}();
+
+exports.detallesNominaMecanico = detallesNominaMecanico;
+
+var nominaAllMecanicos = /*#__PURE__*/function () {
+  var _ref5 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee5(req, res) {
+    var pool, resultado;
+    return _regenerator["default"].wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            _context5.prev = 0;
+            _context5.next = 3;
+            return (0, _database.obtenerConexion)();
+
+          case 3:
+            pool = _context5.sent;
+            _context5.next = 6;
+            return pool.request().input("fechaInicio", req.params.fechaInicio).input("fechaFinal", req.params.fechaFinal).input("idPlaza", req.params.idPlaza).query(_database.querys.nominaAllMecanicos);
+
+          case 6:
+            resultado = _context5.sent;
+            return _context5.abrupt("return", res.json(resultado.recordset));
+
+          case 10:
+            _context5.prev = 10;
+            _context5.t0 = _context5["catch"](0);
+            res.status(500);
+            res.send(_context5.t0.message);
+
+          case 14:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5, null, [[0, 10]]);
+  }));
+
+  return function nominaAllMecanicos(_x9, _x10) {
+    return _ref5.apply(this, arguments);
   };
 }();
 
--- dist/routes/mecanicos.routes.js
+++ dist/routes/mecanicos.routes.js
@@ -14,6 +14,7 @@ var router = (0, _express.Router)(); // obtener todos los mecanicos
 router.get('/mecanicos', _mecanicosController.obtenerMecanicos); //router.get('/mecanico/:id/:factura', reporteMecanico)
 
 router.get('/mecanico/:fechaInicio/:fechaFinal/:idMec', _mecanicosController.nominaMecanico);
+router.get('/mecanico_detalles/:fechaInicio/:fechaFinal/:idMec', _mecanicosController.detallesNominaMecanico);
 router.get('/mecanicos/:fechaInicio/:fechaFinal/:idPlaza', _mecanicosController.nominaAllMecanicos); //router.get('/mecanicosReporte', nominaAllMecanicos)
 // router.get('/mecanicosReporte', nominaAllMecanicos)
 
